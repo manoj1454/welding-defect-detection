3e83ef606585c27e56451b89b9ecc088b3053cf3
Manoj Chary <manojchary1454@gmail.com>
2026-03-19 18:54:04 +0530

    final deployment setup

--- webapp/model_loader.py
+++ webapp/model_loader.py
@@ -1,28 +1,48 @@
 import torch
 import torchvision
 import os
+import gdown
 
 # =========================
 # DEVICE
 # =========================
-
-device = torch.device("cpu")  # Render = CPU only
-
+device = torch.device("cpu")
 
 # =========================
-# PATH FIX (IMPORTANT)
+# PATH
 # =========================
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 MODEL_PATH = os.path.join(BASE_DIR, "..", "models", "weld_final.pth")
 
+# =========================
+# GOOGLE DRIVE FILE ID
+# =========================
+FILE_ID = "13bKS6a9M6qua3ocr_7W4FAJA7xXVf2RH"
+
+MODEL_URL = f"https://drive.google.com/uc?id={FILE_ID}"
+
 
 # =========================
-# LOAD MODEL FUNCTION
+# DOWNLOAD MODEL
 # =========================
+def download_model():
+    os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
 
+    print("Downloading model...")
+
+    gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
+
+    print("Model downloaded successfully!")
+
+
+# =========================
+# LOAD MODEL
+# =========================
 def load_model():
+
+    # 🔥 IMPORTANT FIX: check BEFORE loading
+    if not os.path.exists(MODEL_PATH):
+        download_model()
 
     num_classes = 4
 
